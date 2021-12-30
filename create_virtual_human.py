5e9077e888bc9dd3e8b154bb6273db3439e9d0ce
xiaonian0430 <xiaonian0430@163.com>
2021-12-30 14:56:02 +0800

    修改方法名称

--- create_virtual_human.py
+++ create_virtual_human.py
@@ -4,13 +4,12 @@
 @contact: xiaonian030@163.com
 @time: 2021-12-30 11:30
 """
-from PaddleTools.GAN import FOM
+from utils.gan import driver
 from config.config import MODEL_CONFIG
 
 if __name__ == '__main__':
-    cvh = FOM(MODEL_CONFIG['driver']['input_image'],
-              MODEL_CONFIG['driver']['input_video'],
-              MODEL_CONFIG['driver']['output_video'])
+    cvh = driver(MODEL_CONFIG['driver']['input_image'],
+                 MODEL_CONFIG['driver']['input_video'],
+                 MODEL_CONFIG['driver']['output_video'])
 
     print('已成功创建虚拟人，文件保存在{}'.format(cvh))
-
